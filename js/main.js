eef0034c51f17dd39c202b6908ef251582b688a8
Hong Eungyung <eungyeong217@gmail.com>
2025-01-21 18:26:04 +0900

    상단 배너 이미지 해결 / 다음시간에는 상품 리스트 텍스트 부분 손보기

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  // 상단 노란색 바 상하 슬라이드
   var swiper = new Swiper(".yellow-bar", {
     loop: true,
     autoplay: {
@@ -7,5 +8,25 @@ window.onload = function () {
     },
     slidesPerView: 1,
     direction: "vertical",
+  });
+
+  // 상품 아이템 좌우 슬라이드
+  const itemSwiper = new Swiper(".itemSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1.5,
+    // 첫 화면에 이미지 몇 개 보여줄래
+    spaceBetween:10,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+    },
   });
 };
